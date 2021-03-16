a8fef2da15d2104c1532146379cdf8a30cf14ab2
hanamura <k-hanamura@atware.co.jp>
2021-03-16 17:06:46 +0900

    Windowの場合にプロセス起動時にDETACHED_PROCESSフラグを立てるように変更
    呼び出し元のPythonプログラムをWindowsのバッチファイルから起動した場合に、プロセスをreopenすると標準入力にアクセスできずにOSErrorとなる。
    詳しい原因は不明だが標準入出力を親プロセスから引き継いでいることに起因する問題と思われる。

--- pyknp/juman/process.py
+++ pyknp/juman/process.py
@@ -50,7 +50,7 @@ class Subprocess(object):
             self.process_command = command
             self.process_timeout = timeout
             if platform.system() == "Windows":
-                self.process = Popen(self.process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=self.env, creationflags=subprocess.HIGH_PRIORITY_CLASS, **self.subproc_args)
+                self.process = Popen(self.process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=self.env, creationflags=subprocess.HIGH_PRIORITY_CLASS | subprocess.DETACHED_PROCESS, **self.subproc_args)
             else:
                 self.process = Popen(self.process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=self.env, **self.subproc_args)
             self.encoding = "CP932" if platform.system() == "Windows" else sys.getdefaultencoding()
@@ -97,7 +97,7 @@ class Subprocess(object):
     def reopen(self):
         self.kill()
         if platform.system() == "Windows":
-            self.process = Popen(self.process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=self.env, creationflags=subprocess.HIGH_PRIORITY_CLASS, **self.subproc_args)
+            self.process = Popen(self.process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=self.env, creationflags=subprocess.HIGH_PRIORITY_CLASS | subprocess.DETACHED_PROCESS, **self.subproc_args)
         else:
             self.process = Popen(self.process_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, env=self.env, **self.subproc_args)
 
